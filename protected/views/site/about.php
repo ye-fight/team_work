7aaa6f6ec7ffd553d285f0acf41eabdd384e46be
KidLet <kidlets@qq.com>
2013-07-11 00:19:01 +0800

    培鸿添加 关于我们

---
+++ protected/views/site/about.php
@@ -0,0 +1,74 @@
+<div style="text-align: center; padding: 0 10%">
+	<h1>我们的团队</h1>
+	<table class="table table-hover table-bordered" style="width: 70%; margin: auto;">
+	<thead>
+		<tr>
+			<th>姓名</th>
+			<th>学号</th>
+			<th>职务</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><span class="badge">1</span> 廖智锐</td>
+			<td>20102100185</td>
+			<td>项目及设计组组长</td>
+		</tr>
+		<tr>
+			<td><span class="badge badge-success">2</span> 林友鹏</td>
+			<td>20102100182</td>
+			<td>技术人员</td>
+		</tr>
+		<tr>
+			<td><span class="badge badge-warning">3</span> 周炳佳</td>
+			<td>20102100197</td>
+			<td>技术人员</td>
+		</tr>
+		<tr>
+			<td><span class="badge badge-important">4</span> 黄培鸿</td>
+			<td>20102100191</td>
+			<td>设计人员</td>
+		</tr>
+		<tr>
+			<td><span class="badge badge-info">5</span> 叶江楠</td>
+			<td>20102100184</td>
+			<td>技术组组长</td>
+		</tr>
+	</tbody>
+	</table>
+
+	<div class="section" style="margin-top: 30px;">
+		<div class="page-header">
+			<h2 style="">项目及设计组组长 <small>工作内容</small></h2>
+		</div>
+		<div class="hero-unit"><p>创意构思，需求分析，网站整体设计，功能概要设计，项目管理，任务分配，文档编写。</p></div>
+		
+	</div>
+
+	<div class="section" style="margin-top: 30px;">
+		<div class="page-header">
+			<h2 style="">设计人员 <small>工作内容</small></h2>
+		</div>
+		<div class="hero-unit"><p>创意构思，网站整体设计，界面设计，文档编写，编写“关于我们”部分的代码。</p></div>
+		
+	</div>	
+
+	<div class="section" style="margin-top: 30px;">
+		<div class="page-header">
+			<h2 style="">技术组组长 <small>工作内容</small></h2>
+		</div>
+		<div class="hero-unit"><p>创意构思，网站功能详细设计，网站代码框架以及远程数据库搭建者，git代码管理，代码编写总负责。</p></div>
+		
+	</div>
+
+	<div class="section" style="margin-top: 30px;">
+		<div class="page-header">
+			<h2 style="">技术人员 <small>工作内容</small></h2>
+		</div>
+		<div class="hero-unit"><p>创意构思，根据技术组组长的要求进行网站代码编写，创建数据库表。</p></div>
+		
+	</div>
+
+</div>
+
+
